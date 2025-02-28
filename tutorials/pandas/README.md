ed8b9f21a01eb897442d8104d5369b13242a1592
Baptiste Pesquet <bpesquet@gmail.com>
2025-02-28 19:25:23 +0100

    Start pandas tutorial

--- tutorials/pandas/README.md
+++ tutorials/pandas/README.md
@@ -1,3 +1,25 @@
 # Pandas Tutorial
 
-> Soon!
+This tutorial teaches you the very basics of [pandas](https://pandas.pydata.org). Its complete source code is available [here](test_pandas.py).
+
+## What pandas is
+
+The **pandas** library is dedicated to data analysis and manipulation in Python. It greatly facilitates loading, exploring and processing tabular data files.
+
+## How to import pandas
+
+After [installing pandas](https://pandas.pydata.org/docs/getting_started/index.html), it may be imported into Python code like this.
+
+```python
+import numpy as np
+import pandas as pd
+```
+
+## Basic data structures in pandas
+
+The primary data structures in pandas are implemented as two classes:
+
+- [Series](https://pandas.pydata.org/docs/reference/api/pandas.Series.html#pandas.Series), which represents a single data column: one-dimensional labeled array holding values of any type.
+- [DataFrame](https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html#pandas.DataFrame), which is quite similar to as a relational data table, with rows and named columns. A `DataFrame` contains one or more `Series`. The `DataFrame` is a commonly used abstraction for data manipulation.
+
+... (more to come)
---
+++ tutorials/pandas/test_pandas.py
@@ -0,0 +1,83 @@
+"""
+pandas tutorial code
+"""
+
+import numpy as np
+import pandas as pd
+
+
+def test_data_structures():
+    """
+    Test basic data structures
+    """
+
+    # Create two data Series
+    pop = pd.Series({"CAL": 38332521, "TEX": 26448193, "NY": 19651127})
+    area = pd.Series({"CAL": 423967, "TEX": 695662, "NY": 141297})
+
+    # Create a DataFrame containing the two Series.
+    # The df_ prefix is used to distinguish pandas DataFrames from plain NumPy arrays
+    df_poprep = pd.DataFrame({"Population": pop, "Area": area})
+
+    print(df_poprep)
+
+
+def test_dataset_exploration():
+    """
+    Test functions for loading and exploring a dataset
+    """
+
+    # Use a slightly modified version of the Diabetes dataset.
+    # See https://www4.stat.ncsu.edu/~boos/var.select/diabetes.html
+    dataset_url = "https://raw.githubusercontent.com/bpesquet/mlcourse/refs/heads/main/datasets/diabetes.csv"
+
+    # Fetch dataset from url and load it into a DataFrame
+    df_diabetes = pd.read_csv(dataset_url)
+    print(f"df_diabetes: {df_diabetes.shape}")
+
+    # Print a consise summary of the DataFrame
+    print(df_diabetes.info())
+
+    # Print the first 5 rows
+    print(df_diabetes.head(n=5))
+
+    # Print 10 random rows
+    print(df_diabetes.sample(n=10))
+
+    # Print descriptive statistics about the DataFrame
+    print(df_diabetes.describe())
+
+    # Convert the DataFrame to a plain NumPy array
+    diabetes = df_diabetes.to_numpy()
+    print(f"diabetes (NumPy): {diabetes.shape}, dtype: {diabetes.dtype}")
+
+    # Print the last 5 rows
+    print(df_diabetes[-5:])
+
+    # Print the number of rows for each value of the "SEX" column
+    print(df_diabetes["SEX"].value_counts())
+
+    # Print rows with a blood pressure over 100
+    print(df_diabetes[df_diabetes["BP"] > 100])
+
+    # Average age of all patients
+    avg_age = df_diabetes["AGE"].mean()
+    print(f"Average age: {avg_age:.0f} years")
+
+    # Average age of male patients
+    avg_age_male = df_diabetes[df_diabetes["SEX"] == "M"]["AGE"].mean()
+    print(f"Average age of male patients: {avg_age_male:.0f} years")
+
+    # Select all columns but "Y"
+    df_inputs = df_diabetes.drop("Y", axis="columns")
+    print(f"df_inputs: {df_inputs.shape}")
+
+    # Select "Y" column only
+    df_y = df_diabetes["Y"]
+    print(f"df_y: {df_y.shape}")
+
+
+# Standalone execution
+if __name__ == "__main__":
+    test_data_structures()
+    test_dataset_exploration()
